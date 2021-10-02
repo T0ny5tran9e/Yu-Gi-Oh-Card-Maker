5a2b03751bd2112b6ee60e32ba81f0c502d28c2e
Mohammed Abdul Rahim <rahimkwt2012@gmail.com>
2021-10-02 10:56:44 +0530

    temp for test

---
+++ temp.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter.ttk import *
+
+root = tk.Tk()
+container = tk.Frame(root, bg='cyan')
+canvas = tk.Canvas(container, bg='darkcyan')
+scrollbar = tk.Scrollbar(container, orient="vertical", command=canvas.yview)
+scrollable_frame = tk.Frame(canvas,bg='red')
+
+scrollable_frame.bind(
+    "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+
+canvas.configure(yscrollcommand=scrollbar.set)
+
+for i in range(50):
+    tk.Label(scrollable_frame, text="Sample scrolling label").pack()
+
+container.pack()
+canvas.pack(side="left", fill="both", expand=True)
+scrollbar.pack(side="right", fill="y")
+
+root.mainloop()
